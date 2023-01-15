bbe24ef79170892d931c786b08c666bfc50fc878
NikNik <nickkru@gmail.com>
2023-01-15 17:04:12 +0300

    sem 3 task 4

--- seminar_2/seminar_3/main.js
+++ seminar_2/seminar_3/main.js
@@ -8,6 +8,9 @@
 //     return a + b;
 // }
 // alert(`Результат сложения чисел ${a} и ${b} = ${Math.round(sum(a, b)*100)/100}`)
+
+// task 3
+/*
 const userName = prompt("Enter your name, please");
 function showMessage(name_) {
     console.log(`Привет, ${name_}!`);
@@ -23,3 +26,20 @@ function diolog(bool) {
 diolog(a);
 
 confirm("Вам хорошо живется?") ? alert("Тогда мы идем к вам!") : alert("Ну вы держитесь там!")
+*/
+
+// task 4
+const product = prompt("Введите название продукта")
+switch (product) {
+    case "Мандарины":
+        alert("Мандарины стоят 100 рублей за колограмм")
+        break
+    case "Бананы":
+        alert("Бананы и груши стоят 100 рублей за колограмм")
+        break
+    case "Груши":
+        alert("Бананы и груши стоят 100 рублей за колограмм")
+        break
+    default:
+        alert("Нет такого продукта")
+}
